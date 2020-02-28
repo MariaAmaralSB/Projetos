46f01b378bd7c707ab53c300f0eb5a4451d49be8
Maria Eduarda Amaral <44277668+MariaAmaralSB@users.noreply.github.com>
2020-02-28 10:03:48 -0300

    operadores aritméticos

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -4,6 +4,23 @@ namespace Course
 {
     class Program
     {
-       
+        static void Main(string[] args)
+        {
+            int n1 = 3 + 4 * 2;
+            int n2 = (3 + 4) * 2;
+            int n3 = 17 % 3;
+
+            double a = 1.0, b = -3, c = -4.0;
+
+            double delta = Math.Pow(b, 2.0) - 4.0 * a * c;
+
+            double x1 = (-b + Math.Sqrt(delta)) /  (2.0 * a);
+
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(delta);
+            Console.WriteLine(x1);
+        }
     }
 }
