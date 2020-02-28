083b1525d8dc45c4ef3e9d66fe99760d0d507d58
Maria Eduarda Amaral <44277668+MariaAmaralSB@users.noreply.github.com>
2020-02-28 10:42:07 -0300

    Add files via upload

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -6,21 +6,23 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            int n1 = 3 + 4 * 2;
-            int n2 = (3 + 4) * 2;
-            int n3 = 17 % 3;
+            Console.WriteLine("Qual a hora atual?");
+            int hora = int.Parse(Console.ReadLine());
 
-            double a = 1.0, b = -3, c = -4.0;
+            if (hora < 12)
+            {
+                Console.WriteLine("Bom dia!");
+            }
 
-            double delta = Math.Pow(b, 2.0) - 4.0 * a * c;
+            else if (hora < 18)
+            {
+                Console.WriteLine("Boa Tarde");
+            }
 
-            double x1 = (-b + Math.Sqrt(delta)) /  (2.0 * a);
-
-            Console.WriteLine(n1);
-            Console.WriteLine(n2);
-            Console.WriteLine(n3);
-            Console.WriteLine(delta);
-            Console.WriteLine(x1);
+            else
+            {
+                Console.WriteLine("Boa Noite");
+            }
         }
     }
 }
